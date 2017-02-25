6ff40de7c31bdd8ed54bb51982258bc7459c35b0
giuliojiang <giuliojiang@gmail.com>
2017-02-25 18:25:21 +0000

    dependencies for make and python included

--- script/system_dep_install.sh
+++ script/system_dep_install.sh
@@ -5,4 +5,4 @@ apt-get update
 # Install nodejs
 apt-get -y install curl
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs make python
